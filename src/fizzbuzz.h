2a7c34bfbd5cd851f46440e9b6645f0f8b874ba1
netzego <github@netzego.de>
2023-05-21 20:03:07 +0200

    src: new fizzbuzz.h

---
+++ src/fizzbuzz.h
@@ -0,0 +1,12 @@
+#ifndef FIZZBUZZ_H
+#define FIZZBUZZ_H 1
+
+#define BUFSIZE 16
+
+#define FIZZ 3
+#define BUZZ 5
+#define FIZZBUZZ 15
+
+void fizzbuzz(int num, char *buf);
+
+#endif /* ifndef FIZZBUZZ_H */
